5917ee094582377ffa6696840f1567f1109bfd9b
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-05-17 22:12:01 -0300

    update Scripts

---
+++ html/logout.php
@@ -0,0 +1,11 @@
+<?php
+require('./includes/config.inc.php');
+redirect_invalid_user();
+$_SESSION = array();
+session_destroy();
+setcookie(session_name(), '', time()-300);
+require(MYSQL);
+$page_title = 'Logout';
+include('./includes/header.html');
+echo "<h1>Logged Out</h1><p>Thank you for visiting. You are now logged out. Please come back soon!</p>";
+include('./includes/footer.html');
