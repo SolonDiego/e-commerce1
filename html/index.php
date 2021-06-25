c364b1cf1f1ec8dd31a7ef507ab16632ad908dbc
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-06-24 21:25:25 -0300

    update

--- html/index.php
+++ html/index.php
@@ -6,8 +6,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 }
 include('./includes/header.html');
 ?>
-<h1>Welcome</h1>
-<p class="lead">Welcome to Knowledge is Power, a site dedicated to keeping you up-to-date on the Web security and programming information you need to know. Blah, blah, blah. Yadda, yadda, yadda.</p>
+<h1>Bem-vindo</h1>
+<p class="lead">Bem-vindo ao Saber é poder, um site dedicado a mantê-lo atualizado sobre as informações de segurança e programação da Web que você precisa saber. Blah, blah, blah. Yadda, yadda, yadda.</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur volutpat nunc, eget vulputate quam tristique sit amet. Donec suscipit mollis erat in egestas. Morbi id risus quam. Sed vitae erat eu tortor tempus consequat. Morbi quam massa, viverra sed ullamcorper sit amet, ultrices ullamcorper eros. Mauris ultricies rhoncus leo, ac vehicula sem condimentum vel. Morbi varius rutrum laoreet. Maecenas vitae turpis turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce leo turpis, faucibus et consequat eget, adipiscing ut turpis. Donec lacinia sodales nulla nec pellentesque. Fusce fringilla dictum purus in imperdiet. Vivamus at nulla diam, sagittis rutrum diam. Integer porta imperdiet euismod.</p>
 
 <?php
